6d758f0dc8d3710c7d22cbe7b2acd3efe736f737
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-13 16:22:17 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
 class Book
-  attr_accessor :author, :page_count,
+  attr_accessor :author, :page_count
   attr_reader :title, :genre
   
   GENRES = []
@@ -16,5 +16,4 @@ class Book
     @genre = genre
     GENRES << genre
   end
-
 end
